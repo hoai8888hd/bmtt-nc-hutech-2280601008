0fd89ddddce82d5049225e6916bf47198acca4eb
hoai8888hd <hohoai135@gmail.com>
2025-03-04 08:27:08 +0700

    bai1 helloworld

---
+++ lab01/bai2.py
@@ -0,0 +1,3 @@
+ten = input ("nhap ten cua ban:")
+tuoi= input ("nhap tuoi cua ban:")
+print ("xin chao,toi tên là",ten,"năm nay",tuoi,"tuổi")
---
+++ lab01/lab01.py
@@ -0,0 +1,4 @@
+print("hello world")
+print("My name is HOAI")
+print("Hutech University")
+print("chó hưng")
