4d00a776fce6b6932cdea1cb7fc7a0bf9070038a
hoai8888hd <96343457+hoai8888hd@users.noreply.github.com>
2025-03-25 11:24:08 +0700

    lab03

---
+++ lab03/playfair_cipher.py
@@ -0,0 +1,81 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from ui4.playfair import Ui_MainWindow
+import requests
+
+class MyApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.en.clicked.connect(self.call_api_encrypt)
+        self.ui.de.clicked.connect(self.call_api_decrypt)
+        self.ui.taokey.clicked.connect(self.call_api_taomatran)
+    
+    def call_api_taomatran(self):
+        url = "http://127.0.0.1:5000/api/playfair/creatematrix"
+        payload = {
+            "key": self.ui.key.toPlainText()
+        }
+        try:
+            response = requests.post(url, json=payload)
+            if response.status_code == 200:
+                data = response.json()
+                self.ui.matrix.setPlainText("\n".join(map(str, data["playfair_matrix"])))
+
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Information)
+                msg.setText("Decrypted Successfully")
+                msg.exec_()
+            else:
+                print("Error while calling API")
+        except requests.exceptions.RequestException as e:
+            print("Error: %s" % e.message)
+       
+    def call_api_encrypt(self):
+        url = "http://127.0.0.1:5000/api/playfair/encrypt"
+        payload = {
+            "plain_text": self.ui.plain.toPlainText(),
+            "key": self.ui.key.toPlainText()
+        }
+        try:
+            response = requests.post(url, json=payload)
+            if response.status_code == 200:
+                data = response.json()
+                self.ui.cptext.setPlainText(data["encrypted_text"])
+
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Information)
+                msg.setText("Encrypted Successfully")
+                msg.exec_()
+            else:
+                print("Error while calling API")
+        except requests.exceptions.RequestException as e:
+            print("Error: %s" % e.message)
+    
+    def call_api_decrypt(self):
+        url = "http://127.0.0.1:5000/api/playfair/decrypt"
+        payload = {
+            "cipher_text": self.ui.cptext.toPlainText(),
+            "key": self.ui.key.toPlainText()
+        }
+        try:
+            response = requests.post(url, json=payload)
+            if response.status_code == 200:
+                data = response.json()
+                self.ui.plain.setPlainText(data["decrypted_text"])
+
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Information)
+                msg.setText("Decrypted Successfully")
+                msg.exec_()
+            else:
+                print("Error while calling API")
+        except requests.exceptions.RequestException as e:
+            print("Error: %s" % e.message)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
